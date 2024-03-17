5ea58c28b262987fab7d508e790a4aedff4d1692
Thalia Matos <thalia.matos3197@gmail.com>
2024-03-16 23:47:03 -0300

    first commit

---
+++ imc.js
@@ -0,0 +1,44 @@
+const calcular = document.getElementById('calcular');
+
+function imc() {
+    const nome =document.getElementById('nome').value;
+    const altura =document.getElementById('altura').value;
+    const peso =document.getElementById('peso').value;
+    const resultado =document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        
+        const valorAltura = parseFloat(altura);
+        const valorPeso = parseFloat(peso);
+
+        if (!isNaN(valorAltura) && !isNaN(valorPeso) && valorAltura > 0 && valorPeso > 0) {
+            
+            var valorIMC = (valorPeso/(valorAltura*valorAltura)).toFixed(1);
+
+        }
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5) {
+            classificacao = 'Abaixo do peso.';
+        } else if (valorIMC < 25) {
+            classificacao = 'Com Peso ideal. PARABÉNS!!!'
+        } else if (valorIMC < 30) {
+            classificacao = 'Levemente acima do peso.'
+        } else if (valorIMC < 35) {
+            classificacao = 'Com Obesidade grau I.'
+        } else if (valorIMC < 40) {
+            classificacao = 'Com Obesidade grau II.'
+        } else {
+            classificacao = 'Com Obesidade grau III. CUIDADO!!'
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+    } else {
+        resultado.textContent = 'Preencha todos os campos!!!'
+    }
+
+}
+
+calcular.addEventListener('click', imc);
